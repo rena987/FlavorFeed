57e143ce03e859e1fc1011c30f1ff62920ab80b0
ananyaa7 <ananyaa7@illinois.edu>
2023-11-15 20:43:40 -0600

    user page

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,30 +1,111 @@
 // screens/ProfileScreen.js
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
-  // Add state and functions to handle profile data
+  const navigation = useNavigation();
+
+  const navigateToOtherUserProfile = () => {
+    navigation.navigate('OtherUserProfile'); // Replace with the actual screen name for other users
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Your Profile Screen</Text>
-      {/* Add other UI components based on the image */}
-      {/* ... */}
-    </View>
+    <ScrollView style={styles.container}>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="search for user..."
+        onSubmitEditing={navigateToOtherUserProfile}
+      />
+      <View style={styles.profileSection}>
+        <Icon name="account-circle-outline" size={80} style={styles.profileIcon} />
+        <Text style={styles.username}>Your Username</Text>
+        <Text style={styles.friendsCount}>13 Friends</Text>
+      </View>
+      <View style={styles.preferencesSection}>
+        <Text style={styles.preferencesTitle}>Preferences</Text>
+        <TextInput style={styles.preferencesInput} placeholder="Vegetarian" />
+        <TextInput style={styles.preferencesInput} placeholder="Gluten Free" />
+        <TouchableOpacity style={styles.saveButton}>
+          <Text style={styles.saveButtonText}>Save</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.favoritesSection}>
+        <Text style={styles.favoritesTitle}>Favorites</Text>
+        <Text style={styles.favoriteItem}>Stir Fry @ PAR Residence Hall</Text>
+        <Text style={styles.favoriteItem}>Baked Cod @ LAR</Text>
+        <Text style={styles.favoriteItem}>Yakisoba Noodles @ ISR</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  searchBar: {
+    margin: 10,
+    padding: 8,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 20,
+    fontSize: 16,
+  },
+  profileSection: {
+    alignItems: 'center',
+    margin: 10,
+  },
+  profileIcon: {
+    margin: 10,
+  },
+  username: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  friendsCount: {
+    fontSize: 16,
+    color: '#666',
+  },
+  preferencesSection: {
+    margin: 10,
+  },
+  preferencesTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  preferencesInput: {
+    borderWidth: 1,
+    borderColor: '#ddd',
     padding: 10,
-    // Style as needed based on the image provided
+    borderRadius: 5,
+    marginVertical: 5,
   },
-  title: {
-    // Style for the screen title
+  saveButton: {
+    backgroundColor: '#0000ff',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 5,
   },
-  // Add styles for other elements based on the image provided
-  // ...
+  saveButtonText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  favoritesSection: {
+    margin: 10,
+  },
+  favoritesTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  favoriteItem: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
 });
 
 export default ProfileScreen;
